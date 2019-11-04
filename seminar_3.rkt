d3c40ec81eb5345e9555c6ea5f59e233deba535f
PeterBadzhakov <attentivemaptrout@protonmail.com>
2019-11-04 12:04:31 +0200

    Done for today.

--- seminar_3.rkt
+++ seminar_3.rkt
@@ -44,7 +44,8 @@
         (cond
             ((null? l) result)
             ((list? (car l)) (+ 
-                                (sum-help (car l) res)
+                                (sum-help (car l) 0)
+;                                                 ^Don't repeat results.
                                 (sum-help (cdr l) res)
                              ))
             (else (sum-hlp (cdr l) (+ (car l) res)))
@@ -53,3 +54,35 @@
 
     (sum-hlp l 0)
 )
+
+; Map must keep the original graph layout.
+(define (map l f)
+    (cond
+        ((null? l) l)
+        ((list? (car l)) (cons (map (car l) f) (map (cdr l) f)))
+;                         ^~Cons keeps structure, append would make one big list.
+        (else (cons (f (car l)) (map (cdr l) f)))
+    )
+)
+; NB: (cons `(1 2 3) `()) = `(`(1 2 3)); (cons 5 7) = (5 . 7); (cons 5 (cons 7 ())) = (5 7)
+; ( (1 2 3) . (4 5)) = ((1 2 3) 4 5) != ((1 2 3) (4 5))
+
+(define (my_list? l)
+    (or (null? l) (and (pair? l) (my_list? (cdr l))))
+)
+
+; Matrix: `( `(1 2 3) `(4 5 6) `(7 8 9) )
+(define (matrix? l)
+    (define (m? l len)
+        (or (null? l)
+            (and (list? (car l)) 
+                (= (length (car l)) len)
+                (= (depth (car l)) 1)
+                (m? (cdr l) len)
+            )
+        )
+    )
+
+    ?? ?? ??
+)
+
