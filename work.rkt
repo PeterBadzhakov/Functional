c859286e349278af7054445171889c953b7df9d2
PeterBadzhakov <attentivemaptrout@protonmail.com>
2019-11-14 20:29:03 +0200

    First practicum! Haskell!

--- work.rkt
+++ work.rkt
@@ -1,27 +1,8 @@
-void automaton(bool cond1, bool cond2, bool cond3)
-{
-    if(cond1)
-    {
-        if(cond2)
-        {
-            if(cond3)
-            {
-                goto SthUseful;
-            }
-            else
-            {
-                throw ".....\n";
-            }
-        }
-        else
-        {
-            cerr << "....\n";
-            goto SthUseful;
-        }
-    }
-    else
-    {
-        cerr << "...\n";
-        goto SthUseful;
-    }
-}
+(define (above-diag m)
+    (if (null? m)
+        `()
+        (cons (car m) (above-diag (map cdr (cdr m))))
+    )
+)
+
+
